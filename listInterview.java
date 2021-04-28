fa0f7458abe9b47d70c08e3869ada65d77b4ff4d
mcruz <mcruz@unb.ca>
2021-04-28 19:16:16 -0300

    interview question from cvent

---
+++ listInterview.java
@@ -0,0 +1,16 @@
+/* A hotel has data about which of their hotel rooms are reserved. The data for each reservation
+takes the form of a starting data and an ending date. THey want to be able to find out which
+data ranges have no reservations for any rooms, or "gaps". They have data for all reservations,
+ but only want to find out the gaps between a specific start and end date.
+
+ So, given a list of these reservations, a report start date, and a report end date, return a 
+ list of gaps that occur within that range
+
+*/
+
+//Assuming sorted list
+public List <Gap> findGaps(List<Reservation> reservations, Date startDate, Date endDate) {
+   
+
+
+}
